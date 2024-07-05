054619a641168ff93ccb7d3dd86647aae7c6f0cb
bekhiem8tuoi <91978194+bekhiem8tuoi@users.noreply.github.com>
2024-07-05 18:41:15 +0700

    Update myJs.js

--- js/myJs.js
+++ js/myJs.js
@@ -135,7 +135,7 @@ $(document).ready(function () {
           text: textConfig.text11,
           confirmButtonColor: "#83d0c9",
           onClose: () => {
-            window.location = "https://notepad.vn/sinhnhatcuaVy2";
+            window.location = "https://bekhiem8tuoi.github.io/happybirthdaybeVy/";
           },
         });
       }
